bcab163d5f87283529f9acc855cce23458811bd2
Rich Megginson <richm@stanfordalumni.org>
2010-05-06 08:42:57 -0600

    new file

--- bug572018.py
+++ bug572018.py
@@ -9,8 +9,7 @@ host1 = "localhost.localdomain"
 if len(sys.argv) > 1:
     host1 = sys.argv[1]
 host2 = host1
-cfgport = 1100
-port1 = cfgport + 30
+port1 = 1200
 port2 = port1 + 10
 
 basedn = 'dc=example,dc=com'
@@ -19,7 +18,9 @@ m1replargs = {
 	'bename': "userRoot",
 	'binddn': "cn=replrepl,cn=config",
 	'bindcn': "replrepl",
-	'bindpw': "replrepl"
+	'bindpw': "replrepl",
+    'pd': 5,
+    'tpi': 5
 }
 os.environ['USE_VALGRIND'] = "1"
 m1 = DSAdmin.createAndSetupReplica({
@@ -44,6 +45,39 @@ m2 = DSAdmin.createAndSetupReplica({
 }, m2replargs
 )
 
+print "adding a bunch of unnecessary indexes"
+indexattrs = ['description', 'title', 'facsimileTelephoneNumber', 'street', 'postOfficeBox', 'roomNumber', 'postalCode', 'audio', 'departmentNumber', 'employeeNumber', 'homePhone', 'homePostalAddress', 'manager', 'secretary' ]
+for attr in indexattrs:
+    m1.addIndex(basedn, attr, ['pres', 'eq', 'sub'])
+    m2.addIndex(basedn, attr, ['pres', 'eq', 'sub'])
+
+binattr = "userCertificate;binary"
+binval = ''.join([chr(ii % 256) for ii in xrange(0, 65536)])
+
+basedn2 = "dc=example2,dc=com"
+print "adding another suffix", basedn2
+m1.addSuffix(basedn2)
+m2.addSuffix(basedn2)
+
+print "add several entries to", basedn2
+ent = Entry(basedn2)
+ent.setValues('objectclass', 'extensibleObject')
+m1.add_s(ent)
+m2.add_s(ent)
+
+nusers = 100
+print "add", nusers, "users to", basedn2
+for ii in xrange(0, nusers):
+    uid = "user%03d" % ii
+    dn = "uid=%s,%s" % (uid, basedn2)
+    ent = Entry(dn)
+    ent.setValues('objectclass', 'inetOrgPerson')
+    ent.setValues('sn', 'User%03d' % ii)
+    ent.setValues('cn', 'Test User%03d' % ii)
+    ent.setValues(binattr, binval)
+    m1.add_s(ent)
+    m2.add_s(ent)
+
 initfile = os.environ['PREFIX'] + "/share/dirsrv/data/Example.ldif"
 m1.importLDIF(initfile, '', "userRoot", True)
 
@@ -54,17 +88,74 @@ print "waiting for init to finish"
 m1.waitForReplInit(agmtm1tom2)
 agmtm2tom1 = m2.setupAgreement(m1, m2replargs)
 
-print "delete the first 5 users"
-userdns = []
-ents = m1.search_s(basedn, ldap.SCOPE_SUBTREE, "(uid=*)")
-for ii in range(0, 5):
-    userdns.append(ents[ii].dn)
-    m1.delete_s(ents[ii].dn)
+print "add a new entry parent"
+dn = "cn=parent," + basedn
+ent = Entry(dn)
+ent.setValues('objectclass', 'extensibleObject')
+m1.add_s(ent)
+print "get the uuid of the parent"
+ents = m1.search_s(dn, ldap.SCOPE_BASE, "objectclass=*", ['nsuniqueid'])
+parentuuid = ents[0].nsuniqueid
+print "parent uuid is", parentuuid
 
-print "make sure those users have been deleted on the other server"
+nusers = 100
+print "add", nusers, "users"
+for ii in xrange(0, nusers):
+    uid = "user%03d" % ii
+    dn = "uid=%s,cn=parent,%s" % (uid, basedn)
+    ent = Entry(dn)
+    ent.setValues('objectclass', 'inetOrgPerson')
+    ent.setValues('sn', 'User%03d' % ii)
+    ent.setValues('cn', 'Test User%03d' % ii)
+    ent.setValues(binattr, binval)
+    srv = (m1, m2)[ii % 2]
+    srv.add_s(ent)
+
+print "wait a few seconds for replication to happen . . ."
 time.sleep(10)
-for dn in userdns:
-    while True:
-        try: m2.search_s(dn, ldap.SCOPE_BASE, "(objectclass=*)")
-        except ldap.NO_SUCH_OBJECT: break # was deleted
-        time.sleep(1) # not yet deleted - try again
+
+print "delete newly added users"
+for ii in xrange(0, nusers):
+    uid = "user%03d" % ii
+    dn = "uid=%s,cn=parent,%s" % (uid, basedn)
+    srv = (m1, m2)[ii % 2]
+    srv.delete_s(dn)
+    time.sleep(2)
+
+print "wait a few seconds for replication to happen . . ."
+time.sleep(10)
+
+print "delete the parent"
+dn = "cn=parent," + basedn
+m1.delete_s(dn)
+print "wait a few seconds for replication to happen . . ."
+time.sleep(2)
+
+print "search for the deleted parent entry by uuid"
+filt = '(&(nsUniqueId=%s)(objectclass=nsTombstone))' % parentuuid
+done = False
+while not done:
+    ents = m1.search_s(basedn, ldap.SCOPE_SUBTREE, filt)
+    if not ents:
+        print filt, "not found, sleeping . . ."
+        time.sleep(1)
+    else:
+        print "found tombstone entry", ents[0].dn
+        done = True
+
+print "delete some more entries"
+ents = m1.search_s("ou=people," + basedn, ldap.SCOPE_ONELEVEL)
+for ii in xrange(0, 50):
+    dn = ents[ii].dn
+    srv = (m1, m2)[ii % 2]
+    srv.delete_s(dn)
+    time.sleep(2)
+
+print "parent entry was deleted and tombstoned - now see if it is reaped"
+done = False
+while not done:
+    ents = m1.search_s(basedn, ldap.SCOPE_SUBTREE, filt)
+    done = ents == None
+    if not done:
+        print "Entry", ents[0].dn, "still present, waiting . . ."
+        time.sleep(1)
