cb1af587666d32e9f2e4ddb63c74a6e25aefd793
Rich Megginson <richm@stanfordalumni.org>
2011-10-14 16:01:59 -0600

    added support for callgrind

--- ns-slapd.sh
+++ ns-slapd.sh
@@ -18,48 +18,49 @@ VGSUPPRESS="--suppressions=/tmp/valgrind.supp"
 # use quiet mode if you only want the errors and nothing but the errors
 QUIETMODE="-q"
 
+VG_LOGDIR=${VG_LOGDIR:-/var/tmp}
+
 if [ $USE_PURIFY ]; then
 	LD_LIBRARY_PATH=$NETSITE_ROOT/lib:$NETSITE_ROOT/bin/slapd/lib:$LD_LIBRARY_PATH
 	export LD_LIBRARY_PATH
 	PURIFYOPTIONS="-windows=no -log-file=purify.$$.log -append-logfile=yes -follow-child-processes=no -chain-length=50"
 	export PURIFYOPTIONS
 	SLAPD=./ns-slapd.pure
+	# otherwise, run the same way we run valgrind
+	USE_VALGRIND=1
 fi
 
-if [ $USE_VALGRIND ]; then
-	CHECKCMD="valgrind $QUIETMODE --tool=memcheck --leak-check=yes --leak-resolution=high $VGSUPPRESS $DEMANGLE --num-callers=50 --log-file="
-	# otherwise, run the same way we run purify
-	USE_PURIFY=1
-fi
-
-if [ $USE_GDB ]; then
-#	DISPLAY=:1 ; export DISPLAY
-	argsfile=.gdbinit
-	echo "break main" > $argsfile
-	echo "run $* -d 0" >> $argsfile
-	case "$1" in
-	db2index|suffix2instance|db2archive|archive2db|db2ldif|ldif2db)
-	xterm -bg white -fn 10x20 -sb -sl 2000 -title gdb -e gdb -x $argsfile $SLAPD
-#	/usr/openwin/bin/xterm -bg white -fn 10x20 -sb -sl 2000 -title dbx -e /tools/ns/forte-6.2/bin/dbx -c "stop in main ; run $* -d 0" $SLAPD
-	;;
-	*)
-	xterm -bg white -fn 10x20 -sb -sl 2000 -title gdb -e gdb -x $argsfile $SLAPD &
-#	/usr/openwin/bin/xterm -bg white -fn 10x20 -sb -sl 2000 -title dbx -e /tools/ns/forte-6.2/bin/dbx -c "stop in main ; run $* -d 0" $SLAPD &
-	;;
-	esac
-elif [ $USE_PURIFY ]; then
+if [ "$USE_VALGRIND" -o "$USE_CALLGRIND" ]; then
 	if [ $TET_PNAME ]; then
 		mybase=`basename $TET_PNAME .ksh`
 		mybase=`basename $mybase .sh`
         if [ -z "$mybase" ] ; then
             mybase=unknown
         fi
-        # for some reason, on fedora, --log-file does not append the pid
-		outputfile=/var/tmp/$mybase.vg.$$
+        # valgrind --log-file %p is not supported on all platforms
+		outputfile=${VG_LOGDIR}/$mybase.vg.$$
 	else
-		outputfile=/var/tmp/slapd.vg.$$
+		outputfile=${VG_LOGDIR}/slapd.vg.$$
 	fi
-	CHECKCMD="${CHECKCMD}$outputfile"
+    if [ $USE_VALGRIND ] ; then
+	    CHECKCMD="valgrind $QUIETMODE --tool=memcheck --leak-check=yes --leak-resolution=high $VGSUPPRESS $DEMANGLE --num-callers=50 --log-file=$outputfile"
+    elif [ $USE_CALLGRIND ] ; then
+        CHECKCMD="valgrind $QUIETMODE --tool=callgrind --callgrind-out-file=${VG_LOGDIR}/callgrind.out.$$"
+        USE_VALGRIND=1
+    fi
+fi
+
+if [ $USE_GDB ]; then
+#	DISPLAY=:1 ; export DISPLAY
+	case "$1" in
+	db2index|suffix2instance|db2archive|archive2db|db2ldif|ldif2db)
+	xterm -bg white -fn 10x20 -sb -sl 2000 -title gdb -e gdb --args $SLAPD
+	;;
+	*)
+	xterm -bg white -fn 10x20 -sb -sl 2000 -title gdb -e gdb --args $SLAPD &
+	;;
+	esac
+elif [ $USE_VALGRIND ]; then
 	case "$1" in
 	db2index|suffix2instance|db2archive|archive2db|db2ldif|ldif2db)
 		$CHECKCMD $SLAPD "$@"
