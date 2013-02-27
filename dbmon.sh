77619feacbe31cf19af267aee7888b7c656baa7a
Rich Megginson <richm@stanfordalumni.org>
2013-02-27 13:01:29 -0700

    make dbname col adjustable - improve cache stats layout
    the dbname column width will be a mininum of 7 and a maximum of the
    largest dbname, rather than being a fixed size
    print the entry cache stats on one line, and the DN cache stats below

--- dbmon.sh
+++ dbmon.sh
@@ -21,13 +21,19 @@ dodbmon() {
             fn="entcur entmax entcnt dncur dnmax dncnt"
             split(fn, fields)
             havednstats=0
+            maxdbnamelen=0
         }
         /^nsslapd-dbcachesize/ { dbcachesize=$2 }
         /^nsslapd-db-page-size/ { pagesize=$2 }
         /^dbcachehitratio/ { dbhitratio=$2 }
         /^nsslapd-db-page-ro-evict-rate/ { dbroevict=$2 }
         /^nsslapd-db-pages-in-use/ { dbpages=$2 }
-        /^dn: cn=monitor, *cn=[a-zA-Z0-9][a-zA-Z0-9]*, *cn=ldbm database, *cn=plugins, *cn=config/ { dbname=$5 }
+        /^dn: cn=monitor, *cn=[a-zA-Z0-9][a-zA-Z0-9]*, *cn=ldbm database, *cn=plugins, *cn=config/ {
+            dbname=$5
+            if (tolower(dbname) in dbnames) {
+                len=length(dbname) ; if (len > maxdbnamelen) { maxdbnamelen=len }
+            }
+        }
         /^currententrycachesize/ { stats[dbname,"entcur"]=$2 }
         /^maxentrycachesize/ { stats[dbname,"entmax"]=$2 }
         /^currententrycachecount/ { stats[dbname,"entcnt"]=$2 }
@@ -40,29 +46,29 @@ dodbmon() {
             if (verbose > 1) {
                 print "# dbcachefree - free bytes in dbcache"
                 print "# free% - percent free in dbcache"
-                print "# roevicts - number of read-only pages dropped from cache to make room for other pages - if this is non-zero, it means the dbcache is maxed out and there is page churn"
+                print "# roevicts - number of read-only pages dropped from cache to make room for other pages
+                print "#            if this is non-zero, it means the dbcache is maxed out and there is page churn"
                 print "# hit% - percent of requests that are served by cache"
             }
             print "dbcachefree", free, "free%", (freeratio*100), "roevicts", dbroevict, "hit%", dbhitratio
             if (verbose > 1) {
-                print "# dbname - name of database instance"
-                print "# entries - number of entries in entry cache"
-                print "# efree - number of free bytes in entry cache"
-                print "# efree% - percent free in entry cache"
-                print "# esize - average size of entry in entry cache in bytes (currententrycachesize/currententrycachecount)"
+                print "# dbname - name of database instance - the row shows the entry cache stats"
+                print "# count - number of entries in cache"
+                print "# free - number of free bytes in cache"
+                print "# free% - percent free in cache"
+                print "# size - average size of date in cache in bytes (current size/count)"
                 if (havednstats) {
-                    print "# dns - number of dns in dn cache"
-                    print "# dfree - number of free bytes in dn cache"
-                    print "# dfree% - percent free in dn cache"
-                    print "# dsize - average size of dn in dn cache in bytes (currentdncachesize/currentdncachecount)"
+                    print "# DNcache - the line below the entry cache stats are the DN cache stats"
+                    print "# count - number of dns in dn cache"
+                    print "# free - number of free bytes in dn cache"
+                    print "# free% - percent free in dn cache"
+                    print "# size - average size of dn in dn cache in bytes (currentdncachesize/currentdncachecount)"
                 }
             }
             if (verbose > 0) {
-                printf "%16.16s %10.10s %13.13s %6.6s %7.7s", "dbname", "entries", "efree", "efree%", "esize"
-                if (havednstats) {
-                    printf " %10.10s %13.13s %6.6s %7.7s", "dns", "dfree", "dfree%", "dsize"
-                }
-                printf "\n"
+                if (maxdbnamelen < 7) { maxdbnamelen=7 }
+                fmtstr = sprintf("%%%d.%ds %%10.10s %%13.13s %%6.6s %%7.7s\n", maxdbnamelen, maxdbnamelen)
+                printf fmtstr, "dbname", "count", "free", "free%", "size"
             }
             for (dbn in dbnames) {
                 cur=stats[dbn,"entcur"]
@@ -71,7 +77,8 @@ dodbmon() {
                 free=max-cur
                 freep=free/max*100
                 size=cur/cnt
-                printf "%16.16s %10d %13d %6.1f %7.1f", dbn, cnt, free, freep, size
+                fmtstr = sprintf("%%%d.%ds %%10d %%13d %%6.1f %%7.1f\n", maxdbnamelen, maxdbnamelen)
+                printf fmtstr, dbn, cnt, free, freep, size
                 if (havednstats) {
                     dcur=stats[dbn,"dncur"]
                     dmax=stats[dbn,"dnmax"]
@@ -79,9 +86,8 @@ dodbmon() {
                     dfree=dmax-dcur
                     dfreep=dfree/dmax*100
                     dsize=dcur/dcnt
-                    printf " %10d %13d %6.1f %7.1f", dcnt, dfree, dfreep, dsize
+                    printf fmtstr, "DNcache", dcnt, dfree, dfreep, dsize
                 }
-                printf "\n"
             }
         }
         '
