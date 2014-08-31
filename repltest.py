11bbfe050fc6a6264d3d55c12fc3ee02cc6196f7
Rich Megginson <richm@stanfordalumni.org>
2014-08-31 15:46:05 -0600

    use latest lib389

--- repltest.py
+++ repltest.py
@@ -4,73 +4,74 @@ import sys
 import time
 import ldap
 import logging
-from dsadmin import DSAdmin, Entry, tools
+from lib389 import DirSrv, Entry, tools
+from lib389.tools import DirSrvTools
+from lib389._constants import LOG_REPLICA
 
-logging.getLogger('dsadmin').setLevel(logging.WARN)
-logging.getLogger('dsadmin.tools').setLevel(logging.WARN)
+logging.getLogger('lib389').setLevel(logging.WARN)
+logging.getLogger('lib389.tools').setLevel(logging.WARN)
 
-host1 = "localhost.localdomain"
+basedn = 'dc=example,dc=com'
+rootdn = "cn=directory manager"
+rootpw = 'password'
+replbinddn = "cn=replrepl,cn=config"
+replbindpw = "replrepl"
+host1 = "localhost"
 host2 = host1
 port1 = 1389
 port2 = port1 + 1000
 replid = 1
 
-m1replargs = {
-	'suffix': "dc=example,dc=com",
-	'bename': "userRoot",
-	'binddn': "cn=replrepl,cn=config",
-	'bindcn': "replrepl",
-	'bindpw': "replrepl",
-        'log': True,
-        'id': replid
-}
-replid += 1
-
-#os.environ['USE_DBX'] = "1"
-m1 = tools.DSAdminTools.createAndSetupReplica({
-        'prefix': os.environ.get('PREFIX', None),
-	'newrootpw': 'password',
+createargs = {
+    'prefix': os.environ.get('PREFIX', None),
+	'newrootpw': rootpw,
 	'newhost': host1,
 	'newport': port1,
 	'newinst': 'm1',
-	'newsuffix': 'dc=example,dc=com',
+    'newinstance': 'm1',
+	'newsuffix': basedn,
     'no_admin': True
-}, m1replargs
-)
+}
+
+m1replargs = {
+	'suffix': basedn,
+	'bename': "userRoot",
+	'binddn': replbinddn,
+	'bindpw': replbindpw,
+    'log': True,
+    'id': replid
+}
+replid += 1
+
+agmtargs = {
+    'suffix': basedn,
+    'binddn': replbinddn,
+    'bindpw': replbindpw
+}
+
+#os.environ['USE_DBX'] = "1"
+m1 = tools.DirSrvTools.createInstance(createargs)
 #del os.environ['USE_DBX']
+m1.replicaSetupAll(m1replargs)
 
 m2replargs = m1replargs
 m2replargs['id'] = replid
 replid += 1
 
+createargs['newhost'] = host2
+createargs['newport'] = port2
+createargs['newinst'] = 'm2'
+createargs['newinstance'] = 'm2'
 #os.environ['USE_DBX'] = 1
-m2 = tools.DSAdminTools.createAndSetupReplica({
-        'prefix': os.environ.get('PREFIX', None),
-	'newrootpw': 'password',
-	'newhost': host2,
-	'newport': port2,
-	'newinst': 'm2',
-	'newsuffix': 'dc=example,dc=com',
-    'no_admin': True
-}, m2replargs
-)
+m2 = tools.DirSrvTools.createInstance(createargs)
 #del os.environ['USE_DBX']
-
-initfile = ''
-if os.environ.has_key('SERVER_ROOT'):
-    initfile = "%s/slapd-%s/ldif/Example.ldif" % (m1.sroot,m1.inst)
-else:
-    initfile = "%s/share/dirsrv/data/Example.ldif" % os.environ.get('PREFIX', '/usr')
-#m1.importLDIF(initfile, '', "userRoot", True)
+m2.replicaSetupAll(m2replargs)
 
 print "create agreements and init consumers"
-agmtm1tom2 = m1.setupAgreement(m2, m1replargs)
-m1.startReplication_async(agmtm1tom2)
-print "waiting for init to finish"
-m1.waitForReplInit(agmtm1tom2)
-agmtm2tom1 = m2.setupAgreement(m1, m2replargs)
+agmtm1tom2 = m1.agreement.create(basedn, m2.host, m2.port, agmtargs)
+m1.replica.start_and_wait(agmtm1tom2)
+agmtm2tom1 = m2.agreement.create(basedn, m1.host, m1.port, agmtargs)
 
-basedn = "dc=example,dc=com"
 nents = 5
 m1ents = range(nents)
 m2ents = range(len(m1ents), len(m1ents)+nents+1)
