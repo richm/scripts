4cba39c72e8314ab3d8671eec6e0ec5a8b452207
Rich Megginson <richm@stanfordalumni.org>
2016-09-14 16:27:31 -0600

    allow for testing with kibana

--- oshift-aws-dev.sh
+++ oshift-aws-dev.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+scriptname=`basename $0`
+if [ -f $HOME/.config/$scriptname ] ; then
+    . $HOME/.config/$scriptname
+fi
+
 OS=${OS:-centos7}
 TESTNAME=${TESTNAME:-logging}
 INSTANCE_TYPE=${INSTANCE_TYPE:-c4.large}
@@ -33,23 +38,33 @@ vagrant origin-init --stage inst --os $OS --instance-type $INSTANCE_TYPE "$INSTN
 vagrant up --provider aws
 vagrant sync-origin-aggregated-logging -s -c
 # HACK
-vagrant ssh -c "if [ ! -d /tmp/openshift ] ; then mkdir /tmp/openshift ; fi ; sudo chmod 777 /tmp/openshift"
+vagrant ssh -c "if [ ! -d /tmp/openshift ] ; then mkdir /tmp/openshift ; fi ; sudo chmod 777 /tmp/openshift" -- -n
 # doesn't work in enforcing - see https://github.com/openshift/origin-aggregated-logging/issues/89
 #vagrant ssh -c "sudo setenforce Enforcing"
+# pre-load images to make builds faster
+for image in openshift/origin:v1.2.0 openshift/base-centos7 centos:centos7 openshift/origin-logging-elasticsearch openshift/origin-logging-fluentd openshift/origin-logging-curator openshift/origin-logging-kibana node:0.10.36 centos/ruby-22-centos7 ; do
+    echo pulling image $image ...
+    vagrant ssh -c "docker pull $image" -- -n
+    echo done
+done
+
+# get hostname for kibana
+fqdn=`vagrant ssh-config | awk '/HostName/ {print $2}'`
+kibana_host=kibana.$fqdn
+kibana_ops_host=kibana-ops.$fqdn
 vagrant test-origin-aggregated-logging -d --env GIT_URL=$GIT_URL \
         --env GIT_BRANCH=$GIT_BRANCH \
         --env O_A_L_DIR=$OS_O_A_L_DIR \
         --env USE_LOCAL_SOURCE=true \
         --env OS_ROOT=$OS_ROOT \
         --env ENABLE_OPS_CLUSTER=true \
-        --env DEBUG_FAILURES=true \
-        --env VERBOSE=1
+        --env DO_CLEANUP=false \
+        --env SETUP_ONLY=true \
+        --env VERBOSE=1 \
+        --env PUBLIC_MASTER_HOST=$fqdn \
+        --env KIBANA_HOST=$kibana_host \
+        --env KIBANA_OPS_HOST=$kibana_ops_host
 
 echo Use \'OPENSHIFT_VM_NAME_PREFIX=${OPENSHIFT_VM_NAME_PREFIX} vagrant ssh\' if you want to poke around in the machine
 echo Use \'OPENSHIFT_VM_NAME_PREFIX=${OPENSHIFT_VM_NAME_PREFIX} vagrant modify-instance -r "$INSTNAME"-terminate -s
 echo when you are finished
-echo Otherwise, pkill \'sleep 12345\'
-sleep 12345 || echo sleep killed
-
-vagrant modify-instance -r "$INSTNAME"_terminate -s
-popd
